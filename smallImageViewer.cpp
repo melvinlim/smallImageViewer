e9c711783c498f93e4354284d6820ff7cb267899
melvinlim <dxlim@outlook.com>
2024-04-10 19:41:23 -0700

    removed unused code.  also used MAX_PATH variable defined by microsoft which was apparently 260 instead of value of 256.

--- smallImageViewer.cpp
+++ smallImageViewer.cpp
@@ -6,7 +6,6 @@
 #pragma comment(lib, "gdiplus.lib")
 #define WIN32_LEAN_AND_MEAN
 const char g_szClassName[] = "smallImageViewerClass";
-//const LPCSTR file_open_save_filter = "BMP Files(*.bmp)\0 * .bmp\0All Files(*.*)\0 * .*\0";
 const LPCSTR file_open_save_filter = "Image Files(*.bmp,*.png,*.jpg)\0*.bmp;*.png;*.jpg\0All Files(*.*)\0*.*\0\0";
 const LPCSTR lpstrDefExt = "bmp";	//default extension when file browser is opened.
 
@@ -25,95 +24,12 @@ HBITMAP mLoadImageFile(const WCHAR* filename)
 	return result;
 }
 
-
-BOOL LoadTextFileToEdit(HWND hEdit, LPCTSTR pszFileName)
-{
-	/*
-	HANDLE hFile;
-	BOOL bSuccess = FALSE;
-
-	hFile = CreateFile(pszFileName, GENERIC_READ, FILE_SHARE_READ, NULL,
-		OPEN_EXISTING, 0, NULL);
-	if(hFile != INVALID_HANDLE_VALUE)
-	{
-		DWORD dwFileSize;
-
-		dwFileSize = GetFileSize(hFile, NULL);
-		if(dwFileSize != 0xFFFFFFFF)
-		{
-			LPSTR pszFileText;
-
-			pszFileText = (LPSTR)GlobalAlloc(GPTR, dwFileSize + 1);
-			if(pszFileText != NULL)
-			{
-				DWORD dwRead;
-
-				if(ReadFile(hFile, pszFileText, dwFileSize, &dwRead, NULL))
-				{
-					pszFileText[dwFileSize] = 0; // Add null terminator
-					if(SetWindowText(hEdit, pszFileText))
-						bSuccess = TRUE; // It worked!
-				}
-				GlobalFree(pszFileText);
-			}
-		}
-		CloseHandle(hFile);
-	}
-	return bSuccess;
-	*/
-	return TRUE;
-}
-
-BOOL SaveTextFileFromEdit(HWND hEdit, LPCTSTR pszFileName)
-{
-	/*
-	HANDLE hFile;
-	BOOL bSuccess = FALSE;
-
-	hFile = CreateFile(pszFileName, GENERIC_WRITE, 0, NULL,
-		CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
-	if(hFile != INVALID_HANDLE_VALUE)
-	{
-		DWORD dwTextLength;
-
-		dwTextLength = GetWindowTextLength(hEdit);
-		// No need to bother if there's no text.
-		if(dwTextLength > 0)
-		{
-			LPSTR pszText;
-			DWORD dwBufferSize = dwTextLength + 1;
-
-			pszText = (LPSTR)GlobalAlloc(GPTR, dwBufferSize);
-			if(pszText != NULL)
-			{
-				if(GetWindowText(hEdit, pszText, dwBufferSize))
-				{
-					DWORD dwWritten;
-
-					if(WriteFile(hFile, pszText, dwTextLength, &dwWritten, NULL))
-						bSuccess = TRUE;
-				}
-				GlobalFree(pszText);
-			}
-		}
-		CloseHandle(hFile);
-	}
-	return bSuccess;
-	*/
-	return TRUE;
-}
-
-WCHAR wstr[256];
+WCHAR wstr[MAX_PATH];
 
 BOOL LoadImageFileToEdit(HWND hEdit, LPCTSTR pszFileName) {
 	BOOL bSuccess = FALSE;
-	//g_hbmBall = LoadImage(NULL, "C:\\ball.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
-	//g_hbmBall = (HBITMAP)LoadImage(NULL, pszFileName, IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
 
-//	int wchars_num = MultiByteToWideChar(CP_UTF8, 0, pszFileName, -1, NULL, 0);
-//	WCHAR* wstr = new WCHAR[wchars_num];
-//	MultiByteToWideChar(CP_UTF8, 0, pszFileName, -1, wstr, wchars_num);
-	MultiByteToWideChar(CP_UTF8, 0, pszFileName, -1, wstr, 256);
+	MultiByteToWideChar(CP_UTF8, 0, pszFileName, -1, wstr, MAX_PATH);
 
 	g_hbmBall = mLoadImageFile(wstr);
 	if (g_hbmBall == NULL) {
@@ -134,19 +50,15 @@ void DoFileOpen(HWND hwnd)
 
 	ofn.lStructSize = sizeof(OPENFILENAME);
 	ofn.hwndOwner = hwnd;
-	//ofn.lpstrFilter = "Text Files (*.txt)\0*.txt\0All Files (*.*)\0*.*\0";
-	//ofn.lpstrFilter = "BMP Files (*.bmp)\0*.bmp\0All Files (*.*)\0*.*\0";
 	ofn.lpstrFilter = file_open_save_filter;
 	ofn.lpstrFile = szFileName;
 	ofn.nMaxFile = MAX_PATH;
 	ofn.Flags = OFN_EXPLORER | OFN_FILEMUSTEXIST | OFN_HIDEREADONLY;
-	//ofn.lpstrDefExt = "txt";
 	ofn.lpstrDefExt = lpstrDefExt;
 
 	if(GetOpenFileName(&ofn))
 	{
 		HWND hEdit = GetDlgItem(hwnd, IDC_MAIN_EDIT);
-		//LoadTextFileToEdit(hEdit, szFileName);
 		LoadImageFileToEdit(hEdit, szFileName);
 	}
 }
@@ -160,19 +72,15 @@ void DoFileSave(HWND hwnd)
 
 	ofn.lStructSize = sizeof(OPENFILENAME);
 	ofn.hwndOwner = hwnd;
-	//ofn.lpstrFilter = "Text Files (*.txt)\0*.txt\0All Files (*.*)\0*.*\0";
-	//ofn.lpstrFilter = "BMP Files (*.bmp)\0*.bmp\0All Files (*.*)\0*.*\0";
 	ofn.lpstrFilter = file_open_save_filter;
 	ofn.lpstrFile = szFileName;
 	ofn.nMaxFile = MAX_PATH;
-	//ofn.lpstrDefExt = "txt";
 	ofn.lpstrDefExt = lpstrDefExt;
 	ofn.Flags = OFN_EXPLORER | OFN_PATHMUSTEXIST | OFN_HIDEREADONLY | OFN_OVERWRITEPROMPT;
 
 	if(GetSaveFileName(&ofn))
 	{
 		HWND hEdit = GetDlgItem(hwnd, IDC_MAIN_EDIT);
-		SaveTextFileFromEdit(hEdit, szFileName);
 	}
 }
 
@@ -185,24 +93,6 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 			Gdiplus::GdiplusStartupInput gdiplusStartupInput;			
 			Gdiplus::GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, NULL);
 			g_hbmBall = NULL;
-			/*
-			g_hbmBall = LoadImage(NULL, "C:\\ball.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
-			if (g_hbmBall == NULL)
-				MessageBox(hwnd, "Could not load IDB_BALL!", "Error", MB_OK | MB_ICONEXCLAMATION);
-				*/
-			/*
-			HFONT hfDefault;
-			HWND hEdit;
-
-			hEdit = CreateWindowEx(WS_EX_CLIENTEDGE, "EDIT", "", 
-				WS_CHILD | WS_VISIBLE | WS_VSCROLL | WS_HSCROLL | ES_MULTILINE | ES_AUTOVSCROLL | ES_AUTOHSCROLL, 
-				0, 0, 100, 100, hwnd, (HMENU)IDC_MAIN_EDIT, GetModuleHandle(NULL), NULL);
-			if(hEdit == NULL)
-				MessageBox(hwnd, "Could not create edit box.", "Error", MB_OK | MB_ICONERROR);
-
-			hfDefault = (HFONT)GetStockObject(DEFAULT_GUI_FONT);
-			SendMessage(hEdit, WM_SETFONT, (WPARAM)hfDefault, MAKELPARAM(FALSE, 0));
-			*/
 		}
 		break;
 		/*
@@ -319,5 +209,5 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		DispatchMessage(&Msg);
 	}
 	Gdiplus::GdiplusShutdown(gdiplusToken);
-	return Msg.wParam;
+	return (int)Msg.wParam;
 }
